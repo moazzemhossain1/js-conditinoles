c73269eb388b43f5abc8788c9c55c715e9ec67a0
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-16 05:43:14 +0800

    use in if conditional

---
+++ if.js
@@ -0,0 +1,38 @@
+if(49>50){
+    console.log('i will buy it')
+}else{
+    console.log("I will not buy it")
+
+}
+
+
+
+const pric=4000;
+const mony=4000;
+if(pric<=mony){
+    console.log('I will buy the show.')
+}else{
+    console.log("I will not buy the show.")
+}
+
+
+
+
+const age=16;
+if(age>=18){
+    console.log("You will give the dirver linche");
+}else{
+    console.log("You will not give driver lichence")
+
+}
+
+
+
+
+
+const age1=25;
+if(age1>=25){
+    console.log("Are you eligible to vote")
+}else{
+    console.log("your are not eligible to voat.")
+}
