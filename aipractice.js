7d57548a1d955401a65d5d0424bcce5f1f50fc1c
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-17 18:39:03 +0800

    ai paractice

---
+++ aipractice.js
@@ -0,0 +1,85 @@
+const name=" moazzem ";
+const age="I am 20 years old";
+const conuntry=" Bnagladesh";
+console.log("My name is" +name);
+console.log(age);
+console.log("I live in" +conuntry);
+
+
+
+
+let num1=25;
+let num2=15;
+const addition=num1+num2;
+const subtraction=num1 - num2;
+const multipkication=num1 * num2;
+const dividion=num1 / num2;
+console.log("Addition:" +addition);
+console.log("Subtraction:" +subtraction);
+console.log("Multiplication:" +multipkication);
+console.log("Divison:" +dividion.toFixed(2));
+
+
+
+
+let number=10;
+if(number%2==0){
+    console.log("This is even number")
+}else{
+    console.log("This is odd number")
+}
+
+
+let mumber=-5;
+if(mumber<0){
+    console.log("This positive number")
+}
+else if(mumber<0){
+    console.log("This nagative number")
+}
+else{
+    console.log("This is zero ")
+}
+
+
+
+let age1=10;
+if(age1>=18){
+    console.log("Can vote")
+}
+else{
+    console.log("Cannot vote")
+}
+
+
+
+let number2=41;
+if(number2 >=80  ){
+    console.log("Your grade is A");
+}
+else if(number2 >=60  ){
+    console.log("Your gradeis B")
+
+}
+else if (number2 >=40){
+    console.log("Your grade is C")
+
+}
+else{
+    console.log("You try next year")
+}
+
+
+
+let temperature=35;
+if(temperature >=35){
+    console.log('Very hot');
+}
+else if(temperature >=20){
+    console.log("Normal")
+}
+else{
+    console.log("Cold")
+}
+
+
